346c7d5e6b6fe5a14a25d42c67faf26a4e722f82
alanderekh <alanderekh@gmail.com>
2023-05-04 10:00:48 -0600

    Trabajando en el SignUp

--- static/js/main.js
+++ static/js/main.js
@@ -6,13 +6,57 @@
 function dysplayMenu () {
     let menuContainer = document.querySelector(".container2");
     let menuIcon = document.querySelector(".icon-menu");
-    menuIcon.addEventListener("click",function(){
-        if(menuContainer.classList.contains("extend")){
-            menuContainer.classList.remove("extend");
-        }else{
-            menuContainer.classList.add("extend");
+  
+    if (menuContainer && menuIcon) { 
+      menuIcon.addEventListener("click", function() {
+        if (menuContainer.classList.contains("extend")) {
+          menuContainer.classList.remove("extend");
+        } else {
+          menuContainer.classList.add("extend");
         }
-        
-    })
+      });
+    }
 };
 dysplayMenu();
+  
+
+
+
+function crearSlide(slideHTML, prevBtnHTML, nextBtnHTML) {
+    const slides = document.querySelectorAll('.slide');
+    const prevBtn = document.querySelector('#prevBtn');
+    const nextBtn = document.querySelector('#nextBtn');
+    let index = 0;
+
+    if (slides && prevBtn && nextBtn){
+        slides.forEach((slide, i) => {
+            if (i !== 0) {
+              slide.style.display = 'none';
+            }
+        });
+    
+        prevBtn.addEventListener('click', () => {
+        index = (index - 1 + slides.length) % slides.length;
+        actualizarSlide();
+        });
+    
+        nextBtn.addEventListener('click', () => {
+        index = (index + 1) % slides.length;
+        actualizarSlide();
+        });
+    
+        function actualizarSlide() {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+            slide.style.display = 'block';
+            } else {
+            slide.style.display = 'none';
+            }
+        });
+        }
+    }
+}
+  
+crearSlide('.slide', '#prevBtn', '#nextBtn');
+  
+  
